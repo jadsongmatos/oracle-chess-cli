1e4c3840e5a2a9e50e34c55df37d0280b2e6a10c
jadsongmatos <jadson.g-matos@outlook.com>
2023-06-07 19:47:06 -0300

    façanha: refatorar map_gameover_depth4 para dimensões adicionais.
    - Renomeie `map_gameover_depth3` para `map_gameover_depth4`
    - Modifique a lógica de game over em `map_gameover_depth4` para retornar `[true]` para `moves3dResult`
    - Adicione `moves4dResult` e `moves5dResult` a `map_gameover_depth4`
    - Melhore a eficiência da iteração usando índices específicos na função `loop`
    
    [init.ts]
    - Renomeie `map_gameover_depth3` para `map_gameover_depth4`
    - Modifique `map_gameover_depth4` para retornar `[true]` para game over em `moves3dResult`
    - Adicione `moves4dResult` a `map_gameover_depth4` e remova `game.isGameOver()`
    - Adicionar `moves5dResult` a `moves4dResult` em `map_gameover_depth4`
    - Iterar através de `matrix1[i][1]` em vez de `matrix1[0].length` na função `loop`
    - Iterar através de `matrix1[i][1][j][1][k][1]` em vez de `matrix1[0][0][0].length` na função `loop`
    - Remova o loop `for` comentado na função `loop`.

--- init.ts
+++ init.ts
@@ -2,7 +2,7 @@ import { Chess } from "chess.js";
 
 const aberturas = new Chess();
 
-function map_gameover_depth3(game: Chess) {
+function map_gameover_depth4(game: Chess) {
   return game.moves({ verbose: false }).map((move1d) => {
     game.move(move1d);
     const game_over1d = game.isGameOver();
@@ -19,11 +19,22 @@ function map_gameover_depth3(game: Chess) {
 
       const moves3dResult = game.moves({ verbose: false }).map((move3d) => {
         game.move(move3d);
+        const game_over3d = game.isGameOver()
+        if (game_over3d) {
+          return [true];
+        }
 
-        const moves4dResult = [game.isGameOver()];
+        const moves4dResult = game.moves({ verbose: false }).map((move4d) => {
+          game.move(move4d);
+
+          const moves5dResult = [game.isGameOver()];
+
+          game.undo();
+          return moves5dResult;
+        });
 
         game.undo();
-        return moves4dResult;
+        return [game_over3d, moves4dResult];
       });
 
       game.undo();
@@ -35,29 +46,31 @@ function map_gameover_depth3(game: Chess) {
   });
 }
 
-const aberturas_map: any = map_gameover_depth3(aberturas);
-console.log(aberturas_map)
+const aberturas_map: any = map_gameover_depth4(aberturas);
 
 function loop(matrix1: any) {
   for (let i = 0; i < matrix1.length; i++) {
     //console.log("i:",i,matrix1[i])
-    for (let j = 0; j < matrix1[0].length; j++) {
-      console.log("i:",i,"j:",j,matrix1[i][1][j])
-      for (let k = 0; k < matrix1[0][0].length; k++) {
-        console.log("i:",i,"j:",j,"k:",k,matrix1[i][1][j][1][k])
-        /*for (let n = 0; n < matrix1[0][0][0].length; n++) {
+    for (let j = 0; j < matrix1[i][1].length; j++) {
+      //console.log("i:",i,"j:",j,matrix1[i][1][j])
+      for (let k = 0; k < matrix1[i][1][j][1].length; k++) {
+        //console.log("i:", i, "j:", j, "k:", k, matrix1[i][1][j][1][k][1]);
+        for (let n = 0; n < matrix1[i][1][j][1][k][1].length; n++) {
           console.log(
-            "loop:",
+            "i:",
             i,
+            "j:",
             j,
+            "k:",
             k,
+            "n:",
             n,
-            matrix1[i][j][k][n]
+            matrix1[i][1][j][1][k][1][n][0]
           );
-        }*/
+        }
       }
     }
   }
 }
 
-//loop(aberturas_map);
+loop(aberturas_map);
